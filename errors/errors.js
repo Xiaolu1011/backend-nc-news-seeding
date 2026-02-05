594422db88d8da4e7fba716f8d1a13c7388d1df4
Xiaolu Wang <xiaoluwang@Xiaolus-MacBook-Air.local>
2026-02-05 17:08:55 +0000

    async style updated

---
+++ errors/errors.js
@@ -0,0 +1,28 @@
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status && err.msg) {
+    return res.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
+};
+
+exports.handlePsqlErrors = (err, req, res, next) => {
+  const psqlErrorCodes = {
+    "22P02": { status: 400, msg: "Bad request" },
+    23503: { status: 404, msg: "Not found" },
+    23502: { status: 400, msg: "Bad request" },
+  };
+
+  if (psqlErrorCodes[err.code]) {
+    return res.status(psqlErrorCodes[err.code].status).send({
+      msg: psqlErrorCodes[err.code].msg,
+    });
+  }
+
+  next(err);
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
+  // last resort
+  console.log(err);
+  res.status(500).send({ msg: "Internal server error" });
+};
