08fd1b26726d24e832eb9a6ac445492ce7828406
Xiaolu Wang <jane.zxl1011@gmail.com>
2026-02-21 01:38:24 +0800

    delete auto-seeding

--- listen.js
+++ listen.js
@@ -1,10 +1,7 @@
-const db = require("./db/connection");
-const seed = require("./db/seeds/seed");
-const devData = require("./db/data/development-data");
-
 const app = require("./app");
-const port = 9090;
 
-app.listen(port, () => {
-  console.log(`Listening on ${port}...`);
+const PORT = process.env.PORT || 9090;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
 });
