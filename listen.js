6f7d626cb7f90f0eb8f2f85622d2e9a55260c53c
Xiaolu Wang <jane.zxl1011@gmail.com>
2026-02-21 01:07:32 +0800

    temporarily auto seeding

--- listen.js
+++ listen.js
@@ -1,6 +1,29 @@
-const app = require("./app");
-const port = 9090;
+const seed = require("./db/seeds/seed");
+const devData = require("./db/data/development-data");
 
-app.listen(port, () => {
-  console.log(`Listening on ${port}...`);
-});
+const app = require("./app");
+const db = require("./db/connection");
+const seed = require("./db/seeds/seed");
+const devData = require("./db/data/development-data");
+
+const PORT = process.env.PORT || 9090;
+
+if (process.env.NODE_ENV === "production") {
+  seed(devData)
+    .then(() => {
+      console.log("Production database seeded");
+      app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    })
+    .catch((err) => {
+      console.error("Seeding error:", err);
+    });
+} else {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+}
+
+// const app = require("./app");
+// const port = 9090;
+
+// app.listen(port, () => {
+//   console.log(`Listening on ${port}...`);
+// });
