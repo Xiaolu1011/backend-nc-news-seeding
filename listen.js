225d4f8d814efe3cf8e026fa1b028661c9c50187
Xiaolu Wang <jane.zxl1011@gmail.com>
2026-02-21 01:13:42 +0800

    delete auto-seeding

--- listen.js
+++ listen.js
@@ -3,24 +3,9 @@ const db = require("./db/connection");
 const seed = require("./db/seeds/seed");
 const devData = require("./db/data/development-data");
 
-const PORT = process.env.PORT || 9090;
+const app = require("./app");
+const port = 9090;
 
-if (process.env.NODE_ENV === "production") {
-  seed(devData)
-    .then(() => {
-      console.log("Production database seeded");
-      app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-    })
-    .catch((err) => {
-      console.error("Seeding error:", err);
-    });
-} else {
-  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-}
-
-// const app = require("./app");
-// const port = 9090;
-
-// app.listen(port, () => {
-//   console.log(`Listening on ${port}...`);
-// });
+app.listen(port, () => {
+  console.log(`Listening on ${port}...`);
+});
