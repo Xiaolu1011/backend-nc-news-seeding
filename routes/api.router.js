42a06ed98e2ad32b55fd0d2dbc5c3fc7dd202416
Xiaolu Wang <jane.zxl1011@gmail.com>
2026-02-23 13:10:08 +0800

    added endpoints.json

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,7 +1,8 @@
 const apiRouter = require("express").Router();
+const endpoints = require("../endpoints.json");
 
 apiRouter.get("/", (req, res) => {
-  res.status(200).send({ msg: "Welcome to the API" });
+  res.status(200).send({ endpoints });
 });
 
 const topicsRouter = require("./topic.router");
